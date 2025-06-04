22cab01e03716239f06d026addce80c4cbe44be2
shen <843567305@qq.com>
2025-06-04 13:33:27 +0800

    还原错误 error-stack-parser source-map-js

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import ErrorStackParser from 'error-stack-parser'
+import { findCodeBySourceMap } from './utils/index'
 
 const app = createApp(App)
 
 app.config.errorHandler = (err, vm) => {
-  console.log(err)
-  console.log(vm)
+  const errorStack = ErrorStackParser.parse(err as Error)
+  console.log('🚀 ~ errorStack:', errorStack)
+  if (errorStack.length > 0) {
+    findCodeBySourceMap(errorStack[0])
+  }
 }
 app.use(createPinia())
 app.use(router)
---
+++ src/utils/index.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import sourceMap from 'source-map-js'
+
+const getSourceMap = async (url: string) => {
+  const res = await axios.get(url)
+  return res
+}
+
+export const findCodeBySourceMap = async (stackFrame: any) => {
+  // 获取map文件
+  const sourceData = await getSourceMap(stackFrame.fileName + '.map')
+  const fileContent = sourceData.data
+  // 解析map文件
+  const consumer = await new sourceMap.SourceMapConsumer(fileContent)
+  // 通过报错的位置查找对应的源文件的名称以及报错行号
+  const originalPosition = consumer.originalPositionFor({
+    line: stackFrame.lineNumber,
+    column: stackFrame.columnNumber || 0,
+  })
+  const code = consumer.sourceContentFor(originalPosition.source)
+  console.log('🚀 ~ findCodeBySourceMap ~ code:', code)
+
+  return code
+}
