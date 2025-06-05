c6ca9e1392f8dea3ae5da4727d9a5a15bda251c2
leesen123 <leesen123@users.noreply.github.com>
2025-06-05 05:58:03 +0000

    deploy: e0fd2c6a52cfbe7103e8bd2fe1114a28600d8d40

---
+++ assets/rescordEvens-D7cG3i0F.js
@@ -0,0 +1,2 @@
+import{g as s,h as n}from"./index-CeL72pz7.js";const u=s("record",()=>{const e=n([]);return{events:e,setEvents:t=>{e.value.push(...t)},getEvents:()=>e.value}});export{u};
+//# sourceMappingURL=rescordEvens-D7cG3i0F.js.map
